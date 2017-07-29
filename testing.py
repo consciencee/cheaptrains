b5a9ba004c09fc4cb15be22c48e0d37b0cc936a0
consciencee <triscaideca@yandex.ru>
2017-07-29 12:57:30 +0300

    files without syntax trash

--- testing.py
+++ testing.py
@@ -23,7 +23,6 @@ class localDbMethodsTesting(unittest.TestCase):
 
     st = 51107
     self.assertEqual(dbtools.getBranch(st), 'achj')
-<<<<<<< HEAD
 
 
   def test_isSameBranch(self):
@@ -41,22 +40,14 @@ class localDbMethodsTesting(unittest.TestCase):
 
 
   def test_getSt_Any(self):
-=======
-  
-  def test_getAnyStationFromZone(self):
->>>>>>> e2ef23d78aefdb68d4146ec522db04e5b9c5d39f
     zone = '0'
     self.assertEqual(dbtools.getSt(zone), '45807')
 
     zone = '3'
     self.assertEqual(dbtools.getSt(zone), '46507')
 
-<<<<<<< HEAD
 
   def test_getSt_Conditional(self):
-=======
-  def test_getConditionalStationFromZone(self):
->>>>>>> e2ef23d78aefdb68d4146ec522db04e5b9c5d39f
     zone = '0'
     st = '49307'
     self.assertEqual(dbtools.getSt(zone, st), '45807')
@@ -69,7 +60,6 @@ class localDbMethodsTesting(unittest.TestCase):
     st = '48407'
     self.assertEqual(dbtools.getSt(zone, st), '47507')
 
-<<<<<<< HEAD
 
   def test_getZone(self):
     st = '47507'
@@ -77,8 +67,6 @@ class localDbMethodsTesting(unittest.TestCase):
 
     st = '45807'
     self.assertEqual(dbtools.getZone(st), '0')
-=======
->>>>>>> e2ef23d78aefdb68d4146ec522db04e5b9c5d39f
 
 
 if __name__ == '__main__':
